19980d132e663db9b045e4ce846a2f45929fe168
Anil Shrestha <anill.stha23.as@gmail.com>
2026-01-10 15:31:24 +0930

    main workflow to encrypt, decrypt and verify

--- Question-1/main.py
+++ Question-1/main.py
@@ -1,1 +1,19 @@
-print("setup of first question")
+# main.py
+
+from encryption import encrypt_file
+from decryption import decrypt_file, verify_decryption
+
+def main():
+    shift1 = int(input("Enter shift1 value: "))
+    shift2 = int(input("Enter shift2 value: "))
+
+    encrypt_file(shift1, shift2)
+    print("Encryption completed.")
+
+    decrypt_file(shift1, shift2)
+    print("Decryption completed.")
+
+    verify_decryption()
+
+if __name__ == "__main__":
+    main()
